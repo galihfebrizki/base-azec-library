08bfb21ffe3423127bb1e252330f2edbba39f4d0
galihfebrizki <galih.febrizki@gmail.com>
2020-03-24 15:03:56 +0700

    Signed-off-by: galihfebrizki <galih.febrizki@gmail.com>

--- com/azec/base/baseConfig/baseConfig.go
+++ com/azec/base/baseConfig/baseConfig.go
@@ -4,8 +4,32 @@ import (
 	"base-azec-library/com/azec/base/baseConfig/mysql"
 	"base-azec-library/com/azec/base/baseModel"
 	"database/sql"
+	"encoding/json"
+	"log"
+	"io/ioutil"
+	"os"
 )
 
 func MysqlConnection(databaseModel baseModel.DatabaseModel) *sql.DB {
 	return mysql.MysqlConnection(databaseModel)
+}
+
+func ReadDbModel(pathConfFile string) baseModel.DatabaseModel {
+	var databaseModel baseModel.DatabaseModel
+
+	jsonFile, err := os.Open(pathConfFile)
+
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		log.Println(err.Error())
+	}
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	// we unmarshal our byteArray which contains our
+	// jsonFile's content into 'users' which we defined above
+	json.Unmarshal(byteValue, &databaseModel)
+
+	return databaseModel
 }
